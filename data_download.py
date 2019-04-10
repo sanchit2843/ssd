9667a9494cd5992b507b4aa50443167206a3ee06
sanchit2843 <sanchittanwar75@gmail.com>
2019-04-10 20:22:46 +0530

    Update data_download.py
    Data extraction for pascal voc data added(XML support)

--- data_download.py
+++ data_download.py
@@ -1,10 +1,19 @@
-import wget
-import supportlib.gettingdata as getdata
-import os
-def download(path = None):
-    wget.download('http://ufldl.stanford.edu/housenumbers/train_32x32.mat')
-    wget.download('http://ufldl.stanford.edu/housenumbers/train.tar.gz')
-    if path = None:
-        getdata.tarextract(os.getcwd())
-    else:
-        getdata.tarextract(path)
+import xml.etree.ElementTree as ET
+data = []
+from tqdm import tqdm
+for i in tqdm(os.listdir(root_dir)):
+    path = os.path.join(root_dir,i)
+    tree = ET.parse(path)
+    root = tree.getroot()
+    objects = root.findall('object')
+    a = len(objects)
+    for obj in objects:
+        name = obj.findall('name')[0].text
+        fname = root.findall('filename')[0].text
+        xmax = obj.findall('bndbox')[0].findall('xmax')[0].text
+        xmin = obj.findall('bndbox')[0].findall('xmin')[0].text
+        ymax = obj.findall('bndbox')[0].findall('ymax')[0].text
+        ymin = obj.findall('bndbox')[0].findall('ymin')[0].text
+        data.append([fname,name, xmin, ymin, xmax, ymax])
+df = pd.DataFrame(data, columns=['fname', 'class_name','xmin', 'ymin', 'xmax', 'ymax'])
+data = df.sort_values(by = 'fname')
